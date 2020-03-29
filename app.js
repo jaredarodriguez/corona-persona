6b5227b054f97e48c85c0f5a6d0c49473be09e58
Jared <jaredarodriguez@gmail.com>
2020-03-29 13:07:50 -0400

    Connnected mongoDB and added Persona Model

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express'); 
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express(); 
+
+// Connect to Mongoose 
+mongoose.connect('mongodb://localhost:27017/corona', { useNewUrlParser: true, useUnifiedTopology: true})
+    .then((console.log('MongoDB connected...')))
+    .catch(err => console.log(err))
+
+// Load Idea Model 
+require('./models/Persona');
+const Persona = mongoose.model('persona')
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
@@ -13,7 +23,7 @@ const port = 4000;
 
 // Index Route 
 app.get('/', (req, res) => {
-    const title = 'Welcome'
+    const title = 'Hello Quarantine Quilter'
     res.render('index', {
         title: title
     })
@@ -23,6 +33,13 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about')
 })
+
+// Add Persona form 
+app.get('/personas/add', (req, res) => {
+    res.render('/personas/add')
+})
+
+
 
 app.listen(port, () => {
     console.log(`Server started on ${port}`)
