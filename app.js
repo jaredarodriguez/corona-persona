4b96b0a04a84d265bcd8130febd5cf409595d5be
Jared <jaredarodriguez@gmail.com>
2020-03-30 12:28:37 -0400

    Rendering added personas and added handlebars devDependency

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express'); 
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser'); 
 const mongoose = require('mongoose');
 
 const app = express(); 
+
+//Map to global promise
+mongoose.Promise = global.Promise
 
 // Connect to Mongoose 
 mongoose.connect('mongodb://localhost:27017/corona', { useNewUrlParser: true, useUnifiedTopology: true})
     .then((console.log('MongoDB connected...')))
     .catch(err => console.log(err))
 
-// Load Idea Model 
+// Load Idea Model `
 require('./models/Persona');
 const Persona = mongoose.model('persona')
 
@@ -19,12 +23,16 @@ app.engine('handlebars', exphbs({
 })); 
 app.set('view engine', 'handlebars');
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 const port = 4000; 
 
 // Index Route 
 app.get('/', (req, res) => {
     const title = 'Hello Quarantine Quilter'
-    res.render('index', {
+    res.render('index', { 
         title: title
     })
 })
@@ -34,13 +42,53 @@ app.get('/about', (req, res) => {
     res.render('about')
 })
 
+// Persona Index Page
+
+app.get('/personas', (req, res) => {
+    Persona.find({})
+    .sort({date: 'desc'})
+    .then(personas => {
+        res.render('personas/index', {
+            personas: personas
+        });
+    });
+})
+
 // Add Persona form 
 app.get('/personas/add', (req, res) => {
     res.render('personas/add')
 })
 
 
+// Process form 
+app.post('/personas', (req, res) => {
+    let errors = []; 
+     
+    if(!req.body.name) {
+        errors.push({text: 'Please add a name'})
+    }
+    if(!req.body.description) {
+        errors.push({text: 'Please add description'})
+    }
 
+    if(errors.length > 0) {
+        res.render('personas/add', {
+            errors: errors, 
+            name: req.body.name, 
+            description: req.body.description
+        }) 
+        } else {
+            const newUser = {
+                name: req.body.name, 
+                description: req.body.description
+            }
+            new Persona(newUser)
+            .save()
+            .then(persona => {
+                res.redirect('/personas')
+            })
+        }
+    })
 
 app.listen(port, () => {
     console.log(`Server started on ${port}`)
